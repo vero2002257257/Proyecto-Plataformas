f75dfc49afe346889dcadfb457e89c2fb05ccb41
veronica <105020892+vero2002257257@users.noreply.github.com>
2023-02-24 19:07:09 -0500

    Nuevo

---
+++ src/components/Elements/TestimonialSlider.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Slider from "react-slick";
+import styled from "styled-components";
+// Components
+import TestimonialBox from "../Elements/TestimonialBox";
+
+export default function TestimonialSlider() {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div>
+      <Slider {...settings}>
+        <LogoWrapper className="flexCenter">
+          <TestimonialBox
+            text="Escribe tu mensaje...."
+            author="Enviar"
+          />
+        </LogoWrapper>
+      </Slider>
+    </div>
+  );
+}
+
+const LogoWrapper = styled.div`
+  width: 90%;
+  padding: 0 5%;
+  cursor: pointer;
+  :focus-visible {
+    outline: none;
+    border: 0px;
+  }
+`;
